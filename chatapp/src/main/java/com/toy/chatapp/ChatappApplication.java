d05e6359bb622b53761f2fb30aa4b30fee70f022
nate <nate.koroman@gmail.com>
2025-05-06 01:13:43 -0400

    Added root logger and configured it to print to console and a file

--- chatapp/src/main/java/com/toy/chatapp/ChatappApplication.java
+++ chatapp/src/main/java/com/toy/chatapp/ChatappApplication.java
@@ -1,10 +1,14 @@
 package com.toy.chatapp;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class ChatappApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatappApplication.class, args);
